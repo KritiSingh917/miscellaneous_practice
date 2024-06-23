5cb23322a6a053a157d628dd630caff705e7bba5
kritisingh917 <kritisinghit3024@gmail.com>
2024-06-23 22:26:54 +0530

    changes related to custom search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { CustomAutocompleteComponent } from './custom-autocomplete/custom-autocomplete.component';
+import {FormsModule } from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomAutocompleteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
